8ae9a18f7a2ee44a77cf5a6861768b561bc0ad87
Rishab Parmar <parmar.rishab@gmail.com>
2021-10-05 19:16:30 -0400

    Test

---
+++ src/PlotLineGraph.java
@@ -0,0 +1,35 @@
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+public class PlotLineGraph extends ApplicationFrame {
+
+    public PlotLineGraph(String title) {
+        super(title);
+    }
+
+    public static void main( String[ ] args ) {
+        PlotLineGraph chart = new PlotLineGraph("bruh");
+    }
+
+    public void plot(DefaultCategoryDataset dataset, String chartTitle, String x, String y) {
+//        System.out.println("Dataset: " + dataset);
+        JFreeChart lineChart = ChartFactory.createLineChart(
+                chartTitle,
+                x,y,
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,true,false);
+
+        ChartPanel chartPanel = new ChartPanel( lineChart );
+        chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
+        setContentPane( chartPanel );
+        pack( );
+        RefineryUtilities.centerFrameOnScreen( this );
+        setVisible( true );
+    }
+}
